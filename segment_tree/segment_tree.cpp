8d9118c39e17bc12731dd9707adc9092c7a6f4e0
UshioHayase <UshioHayase@gmail.com>
2026-06-27 18:19:58 +0900

    seg tree

--- segment_tree/segment_tree.cpp
+++ segment_tree/segment_tree.cpp
@@ -1,63 +1,118 @@
-#include <cmath>
-#include <memory>
+#include <concepts>
 
 template <typename T>
-class SegmentTree
+concept INT = std::is_integral_v<T>;
+
+template <typename INT> class SegmentTree
 {
-   public:
-    SegmentTree(const T* const ptr, size_t size)
-        : arr_(new T[1 << (static_cast<int>(ceil(log2(size))) + 1)]), src_(new T[size]), size_(size)
-    {
-        memcpy(src_, ptr, size * sizeof(T));
-        init_segment_tree(ptr, 1, 0, size - 1);
-    }
+    INT* arr_ = nullptr;
+    size_t size_;
 
-    T find(size_t left, size_t right) { return find_(1, 0, size_ - 1, left, right); }
+  public:
+    SegmentTree() = default;
+    ~SegmentTree();
 
-    void replace(size_t idx, T value)
-    {
-        replace_(1, 0, size_ - 1, idx, value - src_[idx]);
-        src_[idx] = value;
-    }
+    void build(INT* data, size_t size);
+    INT query(int start, int end);
+    void replace(INT data, int index);
+    void remove(int index);
 
-   private:
-    T init_segment_tree(const T* const ptr, size_t node, size_t start, size_t end)
-    {
-        if (start == end) return arr_[node] = arr_[start];
-        size_t mid = (start + end) / 2;
-        T left = init_segment_tree(arr_, node * 2, start, mid);
-        T right = init_segment_tree(arr_, node * 2 + 1, mid + 1, end);
-        arr_[node] = left + right;
-
-        return left + right;
-    }
-
-    T find_(size_t node, size_t start, size_t end, size_t left, size_t right)
-    {
-        if (left > end || right < start) return 0;
-        if (left <= start && end <= right) return arr_[node];
-
-        size_t mid = (start + end) / 2;
-        T left_res = find_(node * 2, start, mid, left, right);
-        T right_res = find_(node * 2 + 1, mid + 1, end, left, right);
-
-        return left_res + right_res;
-    }
-
-    void replace_(size_t node, size_t start, size_t end, size_t idx, size_t diff)
-    {
-        if (idx < start || idx > end) return;
-        arr_[node] = arr_[node] + diff;
-
-        if (start != end)
-        {
-            size_t mid = (start + end) / 2;
-            replace_(node * 2, start, end, idx, diff);
-            replace_(node * 2 + 1, mid + 1, end, idx, diff);
-        }
-    }
-
-    T* arr_ = nullptr;
-    T* src_ = nullptr;
-    size_t size_ = 0;
+  private:
+    INT build(INT* data, int start, int end, int idx);
+    INT query(int start, int end, int cur_start, int cur_end, int idx);
+    INT replace(INT data, int idx, int start, int end, int i);
 };
+
+template <typename INT> SegmentTree<INT>::~SegmentTree()
+{
+    if (arr_ != nullptr)
+        delete[] arr_;
+}
+
+template <typename INT> void SegmentTree<INT>::build(INT* data, size_t size)
+{
+    if (arr_ != nullptr)
+        return;
+    arr_ = new INT[size * 4];
+    size_ = size;
+    build(data, 0, size - 1, 1);
+}
+
+template <typename INT>
+INT SegmentTree<INT>::build(INT* data, int start, int end, int idx)
+{
+    int mid = (start + end) / 2;
+    if (start == end)
+        arr_[idx] = data[start];
+    else [[likely]]
+    {
+        INT lhs = build(data, start, mid, 2 * idx);
+        INT rhs = build(data, mid + 1, end, 2 * idx + 1);
+        arr_[idx] = lhs + rhs;
+    }
+
+    return arr_[idx];
+}
+
+template <typename INT> INT SegmentTree<INT>::query(int start, int end)
+{
+    return query(start, end, 0, size_ - 1, 1);
+}
+
+template <typename INT>
+INT SegmentTree<INT>::query(int start, int end, int cur_start, int cur_end,
+                            int idx)
+{
+    int mid = (cur_start + cur_end) / 2;
+
+    if (cur_end < start || cur_start > end)
+    {
+        return 0;
+    }
+    else if (start <= cur_start && cur_end <= end)
+    {
+        return arr_[idx];
+    }
+    else
+    {
+        return query(start, end, cur_start, mid, 2 * idx) +
+               query(start, end, mid + 1, cur_end, 2 * idx + 1);
+    }
+}
+
+template <typename INT> void SegmentTree<INT>::replace(INT data, int idx)
+{
+    replace(data, idx, 0, size_ - 1, 1);
+}
+
+template <typename INT>
+INT SegmentTree<INT>::replace(INT data, int idx, int start, int end, int i)
+{
+    if (idx == start && idx == end)
+    {
+        INT delta = data - arr_[i];
+        arr_[i] = data;
+        return delta;
+    }
+
+    int mid = (start + end) / 2;
+
+    if (start <= idx && idx <= mid)
+    {
+        INT delta = replace(data, idx, start, mid, i * 2);
+        arr_[i] += delta;
+        return delta;
+    }
+    else if (mid < idx && idx <= end)
+    {
+        INT delta = replace(data, idx, mid + 1, end, i * 2 + 1);
+        arr_[i] += delta;
+        return delta;
+    }
+    return 0;
+}
+
+template <typename INT> void SegmentTree<INT>::remove(int idx)
+{
+    replace(0, idx);
+}
